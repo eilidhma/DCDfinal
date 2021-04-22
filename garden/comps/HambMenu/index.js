c8034b680bf4915cbb9d3e40d1bc3cb00f2426ed
Alex Martin <72417540+alexm1612@users.noreply.github.com>
2021-04-21 23:55:26 -0700

    started building the hamburger menu

---
+++ garden/comps/HambMenu/index.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import MainButton from '../MainButton';
+
+const Cont = styled.div`
+    display:flex;
+    flex-direction:column;
+`;
+
+const Menu = ({
+
+
+}) => {
+    return <Cont>
+        
+    </Cont>
+}
+
+export default Menu;
