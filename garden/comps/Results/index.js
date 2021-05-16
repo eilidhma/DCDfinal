8702048c7f1a2bc46e25dc92590a14b3943e144f
zoelake <zoe_lake@icloud.com>
2021-05-15 18:53:31 -0700

    added bee info (not functional)

--- garden/comps/Results/index.js
+++ garden/comps/Results/index.js
@@ -119,6 +119,21 @@ const BeeBadge = styled.div`
   box-shadow: 0 4px 4px 0 rgba(000,000,000,0.50);
 `;
 
+const BeeInfo = styled.div`
+  width:10%;
+  min-width: 120px;
+  border-radius:15px;
+  border:3px solid white;
+  background-color:rgba(255,255,255,0.1);
+  font-size:0.9rem;
+  color:white;
+  text-align:center;
+  position:relative;
+  left: 73%;
+  top: 55px;
+  visibility: visible;
+  `;
+
 
 const Options = ({
   title = "English Lavender",
@@ -135,6 +150,8 @@ const Options = ({
 
 }) => {
   const [open, setOpen] = useState(false);
+  const [bee, setBee] = useState(false);
+
 
   var height = 290, text = "Learn more"
   if (open) {
@@ -142,8 +159,14 @@ const Options = ({
     text = "Hide";
   }
 
+  if (bee) {
+    var beeInfo = document.getElementsByClassName("beeInfo");
+    beeInfo.style.visibility = "hidden";
+  }
+
   return <BeeCont>
-    <BeeBadge><BeeImg src='../bee.png' className="BeeBadge" /></BeeBadge>
+    <BeeInfo className="beeInfo"><p>I support pollination!</p></BeeInfo>
+    <BeeBadge><BeeImg src='../bee.png' className="BeeBadge" onClick={() => setBee(!bee)} /></BeeBadge>
     <Cont height={height}>
       <DescriptionCont>
         <ImgCont><Img src={src} /></ImgCont>
