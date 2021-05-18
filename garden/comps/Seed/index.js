cb3fdfeb463842b8f4ac923711c9e28e79e93e5c
Alex Martin <72417540+alexm1612@users.noreply.github.com>
2021-05-17 23:45:57 -0700

    seed

---
+++ garden/comps/Seed/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Cont = styled.div`
+display:flex;
+justify-content:center;
+align-items:center;
+width:45vw;
+max-width:200px;
+`;
+
+const Seed = () =>{
+    return <Cont>
+
+    </Cont>
+}
+
+export default Seed;
